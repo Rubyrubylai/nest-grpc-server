2832ca1a5282136f5ad5f7ec04a324fd74b37965
Rubyrubylai <r844312@gmail.com>
2023-05-09 15:25:30 +0800

    practice app grpc server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 import { AppService } from './app.service';
+
+interface INumberArray {
+  data: number[]
+}
+
+interface ISumOfNumberArray {
+  sum: number
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @GrpcMethod('AppController', 'Accumulate')
+  accumulate(numberArray: INumberArray): ISumOfNumberArray {
+    return { sum: this.appService.accumulate(numberArray.data)};
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { microserviceOptions } from './grpc.options';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  // app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+
+  // await app.startAllMicroservices();
+  // await app.listen(300);
+  // console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen();
 }
 bootstrap();
